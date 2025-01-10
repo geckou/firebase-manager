f72f6054cc08afe6a6058ebf68791f03732e319f
geckou-shogo <shogo.nojima@geckou.net>
2025-01-10 10:21:31 +0900

    update

---
+++ test/pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { GKTextBox } from '@geckou/vue-ui-components'
+
+
+</script>
+
+<template>
+  <div>
+    <h1>ログイン</h1>
+    <p>ログインページ</p>
+    <GKTextBox></GKTextBox>
+  </div>
+</template>
